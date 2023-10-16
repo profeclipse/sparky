bccc24bef06efc5b9d922f1510e5e53b890c361c
profeclipse <davehunt@me.com>
2023-10-15 20:41:15 -0400

    Added grouping to premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -8,18 +8,21 @@ workspace "sparky"
 
 	outputdir = "%{cfg.buildcfg}-%{cfg.platform}"
 
-	include "dependencies/freeimage"
-	include "dependencies/freetype"
-	include "dependencies/freetype-em"
-	include "dependencies/glad"
-	include "dependencies/glfw"
-	include "dependencies/freetype-gl"
-	include "dependencies/gorilla-audio"
-	include "dependencies/vorbis"
-	include "dependencies/ogg"
-	include "dependencies/spdlog"
-	include "dependencies/jsoncpp"
+	group("Dependencies")
+		include "dependencies/freeimage"
+		include "dependencies/freetype"
+		include "dependencies/freetype-em"
+		include "dependencies/glad"
+		include "dependencies/glfw"
+		include "dependencies/freetype-gl"
+		include "dependencies/gorilla-audio"
+		include "dependencies/vorbis"
+		include "dependencies/ogg"
+		include "dependencies/spdlog"
+		include "dependencies/jsoncpp"
 
-	include "sparky-core"
+	group("Sparky")
+		include "sparky-core"
 	
-	include "sparky-demo"
+	group("Demo")
+		include "sparky-demo"
